8798c687da784f323446975f2c7eef7941b3f926
cetin072 <cetin072072@gmail.com>
2026-08-25 00:32:59 +0900

    chore: refresh PWA cache for save-button layout

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE="worklog-v7";
+const CACHE="worklog-v8";
 const FILES=["/","/index.html","/styles.css","/app.js","/manifest.webmanifest","/icons/icon-192.png","/icons/icon-512.png"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));self.skipWaiting()});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
