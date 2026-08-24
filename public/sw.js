8164d73e161d568c4ae3098d9614dd566bdf763b
cetin072 <cetin072072@gmail.com>
2026-08-24 23:00:07 +0900

    feat: add PWA service worker

---
+++ public/sw.js
@@ -0,0 +1,5 @@
+const CACHE="worklog-v4";
+const FILES=["/","/index.html","/styles.css","/app.js","/manifest.webmanifest","/icons/icon-192.png","/icons/icon-512.png"];
+self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));self.skipWaiting()});
+self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
+self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))});
