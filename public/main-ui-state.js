3a0953ac534caca1ecf51df835c6c0e332d135ca
cetin072 <cetin072072@gmail.com>
2026-09-16 16:52:27 +0900

    feat: load integrated search module

--- public/main-ui-state.js
+++ public/main-ui-state.js
@@ -29,6 +29,15 @@
     } catch {}
   }
 
+  function loadIntegratedSearch() {
+    if (document.querySelector('script[data-worklog-search="1"]')) return;
+    const script = document.createElement("script");
+    script.src = "/search.js?v=20260916-1";
+    script.dataset.worklogSearch = "1";
+    document.head.append(script);
+  }
+
   sanitizeLegacyDraftExtras();
+  loadIntegratedSearch();
   window.WorklogUiPreferences = Object.freeze({ read: readPreferences, save: savePreferences });
 })();
